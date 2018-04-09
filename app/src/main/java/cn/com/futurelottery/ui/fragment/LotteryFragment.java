d730e58879837540d83df0c058f4e0ece55eadfa
doubletan <tantan19900701@163.com>
2018-04-09 16:27:22 +0800

    MeFragment-xml

---
+++ app/src/main/java/cn/com/futurelottery/ui/fragment/LotteryFragment.java
@@ -0,0 +1,10 @@
+package cn.com.futurelottery.ui.fragment;
+
+import cn.com.futurelottery.base.BaseFragment;
+
+/**
+ * Created by tantan on 2018/4/9.
+ */
+
+public class LotteryFragment extends BaseFragment{
+}
