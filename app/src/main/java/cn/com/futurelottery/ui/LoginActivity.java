c1d759f7a3593acc350d7954447216792f792e5f
doubletan <tantan19900701@163.com>
2018-04-10 11:06:32 +0800

    LoginActivity_xml

---
+++ app/src/main/java/cn/com/futurelottery/ui/LoginActivity.java
@@ -0,0 +1,16 @@
+package cn.com.futurelottery.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import cn.com.futurelottery.R;
+import cn.com.futurelottery.base.BaseActivity;
+
+public class LoginActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
